748bd1d609163fd2ed88d2ef47f073e0667856ee
OmarMAbbasi <omarmabbasi@gmail.com>
2019-05-20 15:32:11 -0700

    Finish manager class and moving onto chess

--- Workers/Manager.rb
+++ Workers/Manager.rb
@@ -1,8 +1,13 @@
+require_relative "Employee.rb"
 class Manager < Employee
     
-    def initialize (name, title, salary, boss)
+    def initialize (name, title, salary,&boss)
       @sub_employees = []
-      super 
+      super(name, title, salary, boss)
+    end
+
+    def add_employees(name,title,salary)
+      @sub_employees << Employee.new(name,title,salary,self)
     end
 
     def total_salary
@@ -10,6 +15,7 @@ class Manager < Employee
       @sub_employees.each do |employee|
         total += employee.salary
       end
+      total
     end
 
     def bonus(multiplier)
